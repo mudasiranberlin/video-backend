19db367639ac237933a27eb5afd9817c16258ec2
Mudasir <apple.mudasir@gmail.com>
2026-05-26 09:26:49 +0700

    make

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
  import mongoose from "mongoose";
  import { DB_NAME } from "./constants";
 
+ const connectDB=async()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MOngoDB Connected !! DB HOST: ${connectionInstance}`);
+        
+        
+    } catch (error) {
+        console.log("MONGODB CONNECTION ERROR",error);
+        process.exit(1)
+    }
+ }
 
 
 
 
- 
+
+
 
  
 // import express from "express"
