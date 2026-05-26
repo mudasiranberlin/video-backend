d003fa8e77cd7a527649c37bcbb3b77fc417d5e3
Mudasir <apple.mudasir@gmail.com>
2026-05-26 09:27:31 +0700

    connecting

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
  const connectDB=async()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MOngoDB Connected !! DB HOST: ${connectionInstance}`);
+        console.log(`\n MOngoDB Connected !! DB HOST: ${connectionInstance.connection.host}`);
         
         
     } catch (error) {
