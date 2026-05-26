f4a30ca64dd7c2df4cd788a139d0043ab986efa7
Mudasir <apple.mudasir@gmail.com>
2026-05-26 09:20:58 +0700

    connectdb

---
+++ src/index.js
@@ -0,0 +1,32 @@
+ import mongoose from "mongoose";
+ import { DB_NAME } from "./constants";
+
+
+
+
+
+ 
+
+ 
+// import express from "express"
+// const app =express()
+//  //First Approach connect Db
+//  ( async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("Error",error);
+//             throw error
+            
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`App is Listening on port ${process.env.PORT}`);
+            
+//         })
+//     } catch (error) {
+//         console.error("Error",error);
+//         throw err
+        
+        
+//     }
+//  })()
