7389c65070c461e73973143a088e356b3d1c6cca
Mudasir <apple.mudasir@gmail.com>
2026-05-26 10:33:15 +0700

    usejson

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import dotenv from "dotenv"
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000, ()=>{
+        console.log(`Server is Running at Port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("Mongo db Connection failed",err);
+})
 
 
 
