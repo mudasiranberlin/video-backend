fb7cb0b931d97bf35bf822ac00169d8e3ad8599d
Mudasir <apple.mudasir@gmail.com>
2026-05-27 15:08:02 +0700

    add register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,8 @@ import e from "express";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import {User} from "../models/user.model.js"
+import {uploadOnCLoudinary} from "../utils/cloudinary.js" 
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser= asyncHandler (async (req,res)=>{
     // res.status(200).json(
@@ -46,6 +48,36 @@ const registerUser= asyncHandler (async (req,res)=>{
     const avatarLocalPath=req.files?.avatar[0]?.path;
     const coverImageLocalPath=req.files?.coverImage[0]?.path;
 
+    if (!avatarLocalPath) {
+        throw new ApiError(400,"Avatar File is Required");
+    }
+
+    const avatar= await uploadOnCLoudinary(avatarLocalPath)
+    const coverImage= await uploadOnCLoudinary(coverImageLocalPath);
+    // Avartar // not working //
+    if (!avatar) {
+        throw new ApiError(400,"Avatar file is Required")
+    }
+    const user= await User.create({
+        fullName,
+        avatar:avatar.url,
+        coverImage:coverImage?.url||"",
+        email,
+        password,
+        username:username.tolowerCase()
+    })
+    const createdUser= await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(500,"Something went wrong while create user");
+        
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200,createdUser, "User Registered Sucessfully")
+    )
 
 
 
