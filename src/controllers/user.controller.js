eeaa2b91719c885be1002cd6bdc8637ddf8c3bb6
Mudasir <apple.mudasir@gmail.com>
2026-05-27 17:39:12 +0700

    final

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import e from "express";
+import express from "express";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import {User} from "../models/user.model.js"
