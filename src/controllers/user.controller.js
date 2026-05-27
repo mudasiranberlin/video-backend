8d401d07e7ce36d3a307ed215246d6c947be68a9
Mudasir <apple.mudasir@gmail.com>
2026-05-27 11:30:55 +0700

    post

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,11 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const registerUser= asyncHandler (async (req,res)=>{
+    res.status(200).json(
+        {
+            message:"Mudasir Is Good and Fine Everthing"
+        })
+})
+export{
+    registerUser,
+}
