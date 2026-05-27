0f15ed05a8792c505236ed1ceffc6716095d35ab
Mudasir <apple.mudasir@gmail.com>
2026-05-27 15:43:35 +0700

    change

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -38,7 +38,7 @@ const registerUser= asyncHandler (async (req,res)=>{
         
     }
 
-    const existedUser=User.findOne({
+    const existedUser= await User.findOne({
         $or:[{ username },{ email }]
     })
     if (existedUser) {
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -18,7 +18,7 @@ const userSchema= new Schema(
             lowercase:true,
             trim:true,
         },
-        fullname:{
+        fullName:{
             type:String,
             required:true,
             trim:true,
@@ -46,12 +46,12 @@ const userSchema= new Schema(
             type:String
         }
 
-},{timestamps=true})
+},{timestamps:true})
 
 
 userSchema.pre( "save", async function(next) {
     if (!this.isModified("password")) return next();
-    this.password=bcrypt.hash( this.password,10 )
+    this.password= await bcrypt.hash( this.password,10 )
     next() 
 } )
 
