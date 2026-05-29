f1d09b834d2040b9f485fadab292a056b8addd38
Mudasir <apple.mudasir@gmail.com>
2026-05-29 17:23:51 +0700

    toke

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,9 +1,17 @@
 import { ApiError } from "../utils/ApiError";
 import { asyncHandler } from "../utils/asyncHandler";
+import { User } from "../models/user.model";
 
 export const verifyJWT =asyncHandler(async(req,resizeBy,next)=>{
     const token= req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer","")
     if (!token) {
         throw new ApiError(401,"Unathorized Request");
+    }
+    const decodedToken= jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+
+    const user= await User.findById(decodedToken?._id).select("-password -refreshToken")
+    if (!user) {
+        throw new ApiError(401,"Invalid Access Token");
+        
     }
 }) 
