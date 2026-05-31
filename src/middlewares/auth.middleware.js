b7e459adbd07e4e82eafb95839638c17fdb829a5
Mudasir <apple.mudasir@gmail.com>
2026-05-31 13:41:43 +0700

    catch

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,17 +3,25 @@ import { asyncHandler } from "../utils/asyncHandler";
 import { User } from "../models/user.model";
 
 export const verifyJWT =asyncHandler(async(req,resizeBy,next)=>{
-    const token= req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer","")
-    if (!token) {
-        throw new ApiError(401,"Unathorized Request");
-    }
-    const decodedToken= jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
-
-    const user= await User.findById(decodedToken?._id).select("-password -refreshToken")
-    if (!user) {
-        throw new ApiError(401,"Invalid Access Token");
-
-        // Start new  49:27///
+    try {
+        const token= req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer","")
+        if (!token) {
+            throw new ApiError(401,"Unathorized Request");
+        }
+        const decodedToken= jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+    
+        const user= await User.findById(decodedToken?._id).select("-password -refreshToken")
+        if (!user) {
+            throw new ApiError(401,"Invalid Access Token");
+    
+            // Start new  49:27///
+            
+        }
+        req.user=user;
+        next()
+    } catch (error) {
+        throw new ApiError(401,error?.messsage||"Invalid access token");
+        
         
     }
 }) 
