3ea8622714cfb7f07341c73563498cd53981de6b
Mudasir <apple.mudasir@gmail.com>
2026-06-03 15:02:45 +0700

    feat: add routes for playlists, likes, tweets, comments, subscriptions, and notifications

--- src/app.js
+++ src/app.js
@@ -21,6 +21,12 @@ import userRouter from "./routes/user.routes.js"
 //routes decleration
 
 app.use("/api/v1/users",userRouter)
+app.use("/api/v1/playlists",playlistRouter)
+app.use("/api/v1/likes",likeRouter)
+app.use("/api/v1/tweets",tweetRouter)
+app.use("/api/v1/comments",commentRouter)
+app.use("/api/v1/subscriptions",subscriptionRouter)
+app.use("/api/v1/notifications",notificationRouter)
 
 // It will be like http://localhost:8000/user/login
 
