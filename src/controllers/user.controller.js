7a0ba6471e8881d1774066e87ec321647b9dcb19
Mudasir <apple.mudasir@gmail.com>
2026-06-03 12:06:22 +0700

    feat: add getUserChannelProfile function to retrieve user channel details and subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -302,6 +302,70 @@ const updateUserCoverImage= asyncHandler(async(req,res)=>{
     .json(new ApiResponse(200,user,"Cover Image Updated Sucessfully"))  
 })//
 
+
+const getUserChannelProfile= asyncHandler(async(req,res)=>{
+    const {username}=req.params
+    if (!username?.trim()) {
+        throw new ApiError(400,"Username is Required");
+    }
+    const user= await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount:{
+                    $size:"$subscribers"
+                },
+                channelsSubscribedToCount:{
+                    $size:"$subscribedTo"
+                },
+                isSubscribed: {
+                    $in: [req.user?._id, "$subscribers.subscriber"]
+                },
+                then:true,
+                else:false
+            }
+        },
+        {
+            $project:{
+                fullName:1,
+                username:1,
+                email:1,
+                avatar:1,
+                coverImage:1,
+                subscribersCount:1,
+                channelsSubscribedToCount:1,
+                isSubscribed:1
+            }
+        }
+    ])
+    if(!channel?.length){
+        throw new ApiError(404,"channnel does not exist");
+    }
+    return res
+    .status(200)
+    .json(new ApiResponse(200,channel[0],"User Channel Profile Fetched Sucessfully"))
+})
+
 export {
     registerUser,
     loginUser,
