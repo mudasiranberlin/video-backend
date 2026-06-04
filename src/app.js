45f96d224b1365f50260fe2fd326b17e5f5e2c26
Mudasir <apple.mudasir@gmail.com>
2026-06-04 14:43:11 +0700

    fix: remove unnecessary comment from app.js and fix comment formatting

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 
-app.use(cookieParser())///
+app.use(cookieParser())
 
 
 // routes import
@@ -28,6 +28,6 @@ app.use("/api/v1/comments",commentRouter)
 app.use("/api/v1/subscriptions",subscriptionRouter)
 app.use("/api/v1/notifications",notificationRouter)
 
-// It will be like http://localhost:8000/user/login
+// It will be like http://localhost:8000/user/login//
 
 export {app}
