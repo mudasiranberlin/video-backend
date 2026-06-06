63e93222ed0b14f380fac47b34745bf673451516
Mudasir <apple.mudasir@gmail.com>
2026-06-06 23:39:23 +0700

    feat: define user schema with username, email, and password fields

--- try/user.js
+++ try/user.js
@@ -1,4 +1,19 @@
 import mongoose from "mongoose";
 const userSchema= new mongoose.Schema({
-    
+    username:{
+        type:String,
+        unique:true,
+        lowercase:true,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        lowercase:true,
+        required:true
+    },
+    password:{
+        type:String,
+        required:[true,"Please Enter the Password"]
+    }
 },{timestamps:true})
