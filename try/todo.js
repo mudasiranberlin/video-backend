16d0614a419e3120c596fbfc7f76be99a0f23250
Mudasir <apple.mudasir@gmail.com>
2026-06-09 14:20:16 +0700

    feat: define todo schema with content, complete, createdBy, and subTodos fields

--- try/todo.js
+++ try/todo.js
@@ -1,8 +1,26 @@
 import mongoose from "mongoose";
 
 const todoSchema= new mongoose.Schema({
-    
-},{timestamps:true})
 
+    content:{
+        type:String,
+        required:true
+    },
+    complete:{
+        type:Boolean,
+        default:false
+    },
+    createdBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+
+    subTodos:[
+        {
+            type:String
+        }
+    ]
+
+},{timestamps:true})
 
 export const Todo= mongoose.model("Todo",todoSchema);
