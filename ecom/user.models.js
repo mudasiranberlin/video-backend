6117fa2f510bd7214c509767ef58979cb2ade859
Mudasir <apple.mudasir@gmail.com>
2026-06-09 15:19:17 +0700

    feat: add category model with schema definition

--- ecom/user.models.js
+++ ecom/user.models.js
@@ -1,7 +1,23 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 
 const userSchema= new mongoose.Schema({
-    
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true
+    },
+    email:{
+        type:String,
+        required:true,
+        lowercase:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+
 },{timestamps:true})
 
 export const User= mongoose.model("User",userSchema);
