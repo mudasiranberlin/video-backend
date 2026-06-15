3d3f228a89ad18f5f5499a9fa889758d787aad94
Mudasir <apple.mudasir@gmail.com>
2026-06-15 17:22:59 +0700

    feat: add cloudinary configuration and upload function for image handling

---
+++ back/cloudinary.js
@@ -0,0 +1,23 @@
+import { v2 } from "cloudinary";
+import fs from "fs"
+
+cloudinary.config({ 
+  cloud_name: 'my_cloud_name', 
+  api_key: 'my_key', 
+  api_secret: 'my_secret'
+});
+
+const uploadCloudnary =async(localFilePath)=>{
+    try {
+        if (!localFilePath) return
+        //upload
+        const response= await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        //file has been uploaded sucessfully
+        console.log("File is Uploaded on cloudinary",response.url);
+        return response
+    } catch (error) {
+        
+    }
+}
