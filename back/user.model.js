b732f7fcb3ec84167eb115ae7e551755d197aca8
Mudasir <apple.mudasir@gmail.com>
2026-06-17 16:08:48 +0700

    fix: correct password hashing logic and remove duplicate generateAccessToken method

--- back/user.model.js
+++ back/user.model.js
@@ -49,42 +49,13 @@ const userSchema = new mongoose.Schema({
     }
 },{timestamps:true})
 
-userSchema.pre("save",function (next) {
-    if (this.isModified("password")) return next();
+userSchema.pre("save", async function (next) {
+    if(!this.isModdified("password"))return next();
     this.password=bcrypt.hash(this.password,10)
-    next()
+    next();
 })
-
-userSchema.methods.isPasswordCorrect = async function (password) {
-    await bcrypt.compare(password,this.password)
-}
-userSchema.methods.generateAccessToken =function () {
-    jwt.sign(
-        {
-            _id=this._id,
-            email=this.email,
-            username=this.username,
-            fullNamae=this.fullNamae
-        },
-        process.env.generateAccessToken,
-        {
-            expiresIn:process.env.Access_token_Expiry
-        }
-    )
-    
-}
-userSchema.methods.generateAccessToken =function () {
-
-    jwt.sign(
-        {
-            _id=this._id,
-        },
-        process.env.generateAccessToken,
-        {
-            expiresIn:process.env.Access_token_Expiry
-        }
-    )
-    
+userSchema.methods.isPasswordCorrect =async function (password) {
+    return await bcrypt.compare(password,this.password)
 }
 
 export const User =mongoose.model("User",userSchema)
