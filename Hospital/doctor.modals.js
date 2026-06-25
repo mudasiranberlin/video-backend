eb039bc583466cb64e54a9b48d3d1fe678074117
Mudasir <apple.mudasir@gmail.com>
2026-06-25 15:14:28 +0700

    refactor: clean up doctor schema by removing redundant code and fixing model export

--- Hospital/doctor.modals.js
+++ Hospital/doctor.modals.js
@@ -29,35 +29,3 @@ const doctorSchema = new mongoose.Schema({
 },{timestamps:true})
 
 export const doctor= mongoose.model("doctor",doctor)
-
-
-
-
-import mongoose, { Types } from "mongoose";
-
-const doctorSchema= new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    salary:{
-        type:String,
-        required:true
-    },
-    qualification:{
-        type:String,
-        required:true
-    },
-    experienceInYears:{
-        type:Number,
-        default:0
-    },
-    worksInHospital:[
-        {
-            type:mongoose.Types.ObjectId,
-            ref:"Hospital"
-        }
-    ]
-},{timestamps:true})
-
-export const Doctor= mongoose.model("Doctor",doctorSchema)
